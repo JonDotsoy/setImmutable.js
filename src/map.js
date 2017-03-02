1b75fe22401b20e50e0b2d00548c03dd7cffe4b7
Jonathan Delgado <hi@jon.soy>
2017-03-02 01:03:40 -0300

    Order code

--- src/map.js
+++ src/map.js
@@ -1,21 +1,18 @@
 const set = require('./setImmutable')
 
 function mapSetImmutable (objArg, mapping) {
-
   if (Array.isArray(mapping)) {
-    return [objArg, ...mapping].reduce((currentObj, [ path, value ] ) => {
+    return [objArg, ...mapping].reduce((currentObj, [ path, value ]) => {
       return set(currentObj, path, value)
     })
-  } else if ('function' === typeof(mapping)) {
+  } else if (typeof (mapping) === 'function') {
     const nextMapping = []
 
     const r = mapping((...arg) => { nextMapping.push(arg) })
 
     return mapSetImmutable(objArg, nextMapping)
   }
-
 }
-
 
 exports = module.exports = mapSetImmutable
 
